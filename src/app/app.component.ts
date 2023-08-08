19aaa522fccd4906b3055d32e2b4d9c7d518c6d7
danial <danialmoosavi69@gmail.com>
2023-08-08 21:07:31 +0330

    angular card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts=[{
+    title:'tree',
+    imageUrl:'assets/images/tree.jpg',
+    username:'tree',
+    content:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo mollitia aut possimus adipisci corrupti quam pariatur magnam est repellat libero, deleniti, sequi necessitatibus ullam voluptatibus veritatis dolore hic? Corporis, mollitia.'
+  },
+  {
+    title:'mountain',
+    imageUrl:'assets/images/mountain.jpg',
+    username:'mountain',
+    content:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo mollitia aut possimus adipisci corrupti quam pariatur magnam est repellat libero, deleniti, sequi necessitatibus ullam voluptatibus veritatis dolore hic? Corporis, mollitia.'
+  },
+  {
+    title:'bike',
+    imageUrl:'assets/images/bike.jpg',
+    username:'bike',
+    content:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo mollitia aut possimus adipisci corrupti quam pariatur magnam est repellat libero, deleniti, sequi necessitatibus ullam voluptatibus veritatis dolore hic? Corporis, mollitia.'
+  },
+  
+]
 }
